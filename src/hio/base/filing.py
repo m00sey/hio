77d459602723cb012834a2bf5ba9df7f89fe7e64
Kevin Griffin <griffin.kev@gmail.com>
2021-12-06 10:46:39 -0500

    pyinstaller support
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- src/hio/base/filing.py
+++ src/hio/base/filing.py
@@ -6,6 +6,7 @@ hio.base.filing module
 import os
 import stat
 import shutil
+import sys
 import tempfile
 from contextlib import contextmanager
 
@@ -88,7 +89,8 @@ class Filer():
         stat.S_IWUSR Owner has write permission.
         stat.S_IXUSR Owner has execute permission.
     """
-    HeadDirPath = "/usr/local/var"  # default in /usr/local/var
+    # in the case of a pyinstaller packaged application we default to the pyinstaller app location
+    HeadDirPath = sys._MEIPASS if getattr(sys, 'frozen', False) and getattr(sys, '_MEIPASS', '') else "/usr/local/var"
     TailDirPath = "hio"
     CleanTailDirPath = "hio/clean"
     AltHeadDirPath = "~"  # put in ~ as fallback when desired not permitted
